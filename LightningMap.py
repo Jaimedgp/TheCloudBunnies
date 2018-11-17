55538b9d4807eef20b9640a82965d0297b683f76
Jaime <jaime.diez.gp@gmail.com>
2018-11-17 13:44:37 +0100

    Convolve

--- LightningMap.py
+++ LightningMap.py
@@ -2,10 +2,12 @@ import wget
 import numpy as np
 from scipy.misc import imread, imresize
 import matplotlib.pyplot as plt
+import scipy.signal as signal
 
 
 def getMapLightning():
-    url='http://www.aemet.es/imagenes_d/eltiempo/observacion/rayos/201811162200_r79g.gif'
+    #url='http://www.aemet.es/imagenes_d/eltiempo/observacion/rayos/201811162200_r79g.gif'
+    url = 'http://2.bp.blogspot.com/-MViQnuVJCXg/TcZOE0t5yEI/AAAAAAAACYI/igevZeeZwOI/s1600/rayos.gif'
     image = wget.download(url)
     image = imread(image)
 
@@ -26,4 +28,27 @@ def getMapLightning():
                 maps[i][j] = 0
             else:
                 maps[i][j] = 1
-    return maps
+    return maps[:,:,0]
+
+
+def Convolve(matrix1, matrix2):
+
+    convolve = np.zeros((368, 368))
+
+    for i in range(len(matrix1)):
+        for j in range(len(matrix1[0])):
+            convolve[i][j] = matrix1[i][j]*matrix2[i][j]
+
+    return convolve
+
+#############################
+#####         MAIN      #####
+#############################
+
+Map = getMapLightning()
+filters = [ [255 for i in range(0,368)] for j in range(0,368)]
+
+P = Convolve(Map, filters)
+
+plt.imshow(P)
+plt.show()
